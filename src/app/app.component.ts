8892e8ab6f3bf9c8c55a7c8fb3925c519fa693f7
Michael Utz <michaelutz@utzMBP.local>
2017-03-25 22:29:47 -0500

    Setup basic brand styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
+import { Subscription } from 'rxjs/Rx';
+import { OnDestroy, OnInit } from '@angular/core/core';
 import { Component } from '@angular/core';
+import { BrandingService } from 'app/services/branding.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Kanser Forumu';
+export class AppComponent implements OnInit, OnDestroy {
+  title: string;
+
+  private _titleSub: Subscription;
+
+  constructor(
+    private _brand: BrandingService
+  ) { }
+
+  ngOnInit() {
+    this._titleSub = this._brand.siteName$
+      .subscribe(x => this.title = x);
+  }
+
+  ngOnDestroy() {
+    this._titleSub.unsubscribe();
+  }
 }
