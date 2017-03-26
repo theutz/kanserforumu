63ba7a722547d1af57275933f47b8bdf7e4fbd98
Michael Utz <michaelutz@utzMBP.local>
2017-03-25 22:01:19 -0500

    Provided title service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { BrandingService } from './services/branding.service';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -14,7 +15,9 @@ import { AppComponent } from './app.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [
+    BrandingService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
