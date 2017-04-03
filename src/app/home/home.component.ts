e3cae5c1bc60a6d98521002d47c80115d45ffb99
Michael Utz <michaelutz@utzMBP.local>
2017-04-02 21:50:22 -0500

    Fixed carousel image on home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
+import { TranslateService } from '@ngx-translate/core';
 import { Component, OnInit } from '@angular/core';
+import { Slides, Slide } from './slides';
 
 @Component({
   selector: 'app-home',
@@ -6,12 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  slides: Slides;
 
   constructor(
-  ) {
-  }
+    private _translate: TranslateService
+  ) { }
 
   ngOnInit() {
+    this._setSlides();
+  }
+
+  private _setSlides() {
+    this._translate
+      .get('HOME.SLIDES')
+      .subscribe(slides => {
+        this.slides = slides;
+      });
   }
 
 }
---
+++ src/app/home/slides.ts
@@ -0,0 +1,7 @@
+export type Slides = Slide[];
+
+export interface Slide {
+  title: string;
+  subtitle: string;
+  photoUrl: string;
+}
