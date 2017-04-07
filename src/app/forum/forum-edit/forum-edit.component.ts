d8094b47d849802863fe9fab51ecb31e09435934
Michael Utz <michaelutz@utzMBP.local>
2017-04-06 22:29:33 -0500

    Setup forum subscription

--- src/app/forum/forum-edit/forum-edit.component.ts
+++ src/app/forum/forum-edit/forum-edit.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ForumService } from '../../services/forum.service';
+import { Observable, Subscription } from 'rxjs/Rx';
+import { Forum } from '../../services/forum';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-forum-edit',
   templateUrl: './forum-edit.component.html',
   styleUrls: ['./forum-edit.component.scss']
 })
-export class ForumEditComponent implements OnInit {
+export class ForumEditComponent implements OnInit, OnDestroy {
+  private _key$sub: Subscription;
 
-  constructor() { }
+  forum: Forum;
+  private _forum$: Observable<Forum>;
+  private _forum$sub: Subscription;
+
+  constructor(
+    private _fs: ForumService,
+    private _route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this._key$sub = this._createForum$();
+    this._forum$sub = this._subscribeToForum();
   }
 
+  ngOnDestroy() {
+    this._key$sub.unsubscribe();
+    this._forum$sub.unsubscribe();
+  }
+
+  private _subscribeToForum(): Subscription {
+    return this._forum$.subscribe(forum => this.forum = forum);
+  }
+
+  private _createForum$(): Subscription {
+    return this._getForumKeyFromRoute().subscribe(key => this._forum$ = this._fs.get(key));
+  }
+
+  private _getForumKeyFromRoute(): Observable<string> {
+    return this._route.paramMap.map(map => map.get('id'));
+  }
 }
