3b9f508e8022baa9f1db595a04f0ab2abb1bd71b
Michael Utz <michaelutz@utzMBP.local>
2017-04-16 14:20:37 -0500

    Basic discussion view loaded

--- src/app/forum/discussion-view/discussion-view.component.ts
+++ src/app/forum/discussion-view/discussion-view.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs/Rx';
+import { Discussion } from '../../services/discussion';
+import { DiscussionsService } from '../../services/discussions.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-discussion-view',
   templateUrl: './discussion-view.component.html',
   styleUrls: ['./discussion-view.component.scss']
 })
-export class DiscussionViewComponent implements OnInit {
+export class DiscussionViewComponent implements OnInit, OnDestroy {
+  discussion: Discussion;
 
-  constructor() { }
+  private _unsubscriber = new Subject<void>();
+  private _destroy$ = this._unsubscriber.asObservable();
+
+  constructor(
+    private _discussionService: DiscussionsService,
+    private _router: Router,
+    private _route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this._route.paramMap
+      .takeUntil(this._destroy$)
+      .map(map => map.get('discussionId'))
+      .switchMap(key => this._discussionService.get(key))
+      .do(d => this.discussion = d)
+      .subscribe(console.log);
   }
 
+  ngOnDestroy() {
+    this._unsubscriber.next(null);
+    this._unsubscriber.complete();
+  }
 }
