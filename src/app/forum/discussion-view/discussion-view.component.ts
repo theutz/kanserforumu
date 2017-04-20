e39178d8bb73a86beff71223bc6eb734dec53b0f
Michael Utz <michaelutz@utzMBP.local>
2017-04-20 12:33:39 -0500

    fix: rm console.log()

--- src/app/forum/discussion-view/discussion-view.component.ts
+++ src/app/forum/discussion-view/discussion-view.component.ts
@@ -77,7 +77,6 @@ export class DiscussionViewComponent implements OnInit, OnDestroy {
     }
 
     addComment() {
-        console.log(this.user);
         this._commentsService.add(this.newComment)
             .first()
             .subscribe(x => {
